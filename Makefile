133d826961da0d14a288bb92a2cc0ead31b49ffb
Jonathan Guthrie <jguthrie@brokersys.com>
2016-06-12 20:20:08 -0500

    Add a potentiometer-based speed control and a display.
    It's all still very crude and disorganized, but organization is coming.

--- Makefile
+++ Makefile
@@ -1,5 +1,7 @@
 BOARD_TAG    = uno
-ARDUINO_LIBS = 
+# The NewliquidCrystal library is available here:
+# https://bitbucket.org/fmalpartida/new-liquidcrystal/wiki/Home
+ARDUINO_LIBS = NewliquidCrystal Wire
 USER_LIB_PATH = ${PWD}/libraries
 MONITOR_PORT = /dev/ttyACM0
 
--- new_keyer.ino
+++ new_keyer.ino
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 
+#include <LiquidCrystal_I2C.h>
+
 #define LEFT_PADDLE 5
 #define RIGHT_PADDLE 2
 
@@ -10,7 +12,11 @@
 
 #define SIDETONE_FREQUENCY 800
 
-#define WPM 16
+#define MIN_WPM 5L
+#define MAX_WPM 40L
+int potPin = A0;
+int potValue = 0;
+char buffer[100];
 
 unsigned dot_twitches;
 unsigned dash_twitches;
@@ -23,26 +29,43 @@ typedef enum keyer_state { KEY_DIT, KEY_DAH, KEY_UP } keyer_state_t;
 keyer_state_t keyer_state;
 keyer_state_t next_keyer_state;
 
+LiquidCrystal_I2C display(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+
+unsigned find_wpm() {
+    unsigned wpm;
+    // The WPM is MIN_WPM + (MAX_WPM - MIN_WPM) * 1024 / potValue;
+    potValue = analogRead(potPin);
+    wpm = MIN_WPM + ((MAX_WPM - MIN_WPM) * potValue + 512L) / 1024L;
+    display.home();
+    sprintf(buffer, "%2d", wpm);
+    display.write((const uint8_t*) buffer, strlen(buffer));
+    return wpm;
+}
+
 void setup () {
-     pinMode(LEFT_PADDLE, INPUT);
-     digitalWrite(LEFT_PADDLE, HIGH);
-     pinMode(RIGHT_PADDLE, INPUT);
-     digitalWrite(RIGHT_PADDLE, HIGH);
+    unsigned wpm;
+    pinMode(LEFT_PADDLE, INPUT);
+    digitalWrite(LEFT_PADDLE, HIGH);
+    pinMode(RIGHT_PADDLE, INPUT);
+    digitalWrite(RIGHT_PADDLE, HIGH);
 
-     pinMode(SIDETONE, OUTPUT);
-     digitalWrite(SIDETONE, LOW);
+    pinMode(SIDETONE, OUTPUT);
+    digitalWrite(SIDETONE, LOW);
 
-     dot_twitches = 1000/WPM;
-     dash_twitches = 3000/WPM;
-     word_twitches = 7000/WPM;
-     last_ms = millis();
-     next_state_transition_ms = last_ms;
-     keyer_state = KEY_UP;
-     next_keyer_state = KEY_UP;
+    last_ms = 100 + millis();
+    next_state_transition_ms = last_ms;
+    keyer_state = KEY_UP;
+    next_keyer_state = KEY_UP;
+
+    display.begin(20, 4, LCD_5x8DOTS);
+    display.on();
+    display.clear();
+    display.noCursor();
 }
 
 void loop() {
     unsigned long now = millis();
+    unsigned wpm;
 
     if (now >= next_state_transition_ms) {
 	switch(keyer_state) {
@@ -94,4 +117,9 @@ void loop() {
 	    break;
 	}
     }
+
+    wpm = find_wpm();
+    dot_twitches = 1000/wpm;
+    dash_twitches = 3000/wpm;
+    word_twitches = 7000/wpm;
 }	
