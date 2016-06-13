bfdb68541c0f160239a8b5691850c5e748419815
Jonathan Guthrie <jguthrie@brokersys.com>
2016-06-12 19:37:41 -0500

    This is a rudimentary iambic keyer.
    It doesn't actually key anything but the sidetone.  I'll fix that later, after
    I do some reorganization along the lines of having a "key output" class that
    basically handles everything.
    
    The secret to getting responsiveness out of the paddles is to set their output
    high after setting the direction to input.  I suspect that actually causes the
    pull up resistors to work.  I didn't realize this until I reviewed Anthony
    Good's code.

--- new_keyer.ino
+++ new_keyer.ino
@@ -5,21 +5,93 @@
 
 #define SIDETONE 4
 
+#define PTT_1    13
+#define KEY_OUT_1 11
+
 #define SIDETONE_FREQUENCY 800
+
+#define WPM 16
+
+unsigned dot_twitches;
+unsigned dash_twitches;
+unsigned word_twitches;
+unsigned long last_ms;
+unsigned long next_state_transition_ms;
+
+typedef enum keyer_state { KEY_DIT, KEY_DAH, KEY_UP } keyer_state_t;
+
+keyer_state_t keyer_state;
+keyer_state_t next_keyer_state;
 
 void setup () {
      pinMode(LEFT_PADDLE, INPUT);
+     digitalWrite(LEFT_PADDLE, HIGH);
      pinMode(RIGHT_PADDLE, INPUT);
+     digitalWrite(RIGHT_PADDLE, HIGH);
+
      pinMode(SIDETONE, OUTPUT);
+     digitalWrite(SIDETONE, LOW);
+
+     dot_twitches = 1000/WPM;
+     dash_twitches = 3000/WPM;
+     word_twitches = 7000/WPM;
+     last_ms = millis();
+     next_state_transition_ms = last_ms;
+     keyer_state = KEY_UP;
+     next_keyer_state = KEY_UP;
 }
 
 void loop() {
-    while(1) {
-	if (!(digitalRead(LEFT_PADDLE) || digitalRead(RIGHT_PADDLE))) {
-	    tone(SIDETONE, SIDETONE_FREQUENCY);
-	}
-	else {
+    unsigned long now = millis();
+
+    if (now >= next_state_transition_ms) {
+	switch(keyer_state) {
+	case KEY_DIT:
 	    noTone(SIDETONE);
+	    next_state_transition_ms = now + dot_twitches;
+	    keyer_state = KEY_UP;
+	    if (!digitalRead(RIGHT_PADDLE)) {
+		next_keyer_state = KEY_DAH;
+	    }
+	    break;
+
+	case KEY_DAH:
+	    noTone(SIDETONE);
+	    next_state_transition_ms = now + dot_twitches;
+	    keyer_state = KEY_UP;
+	    if (!digitalRead(LEFT_PADDLE)) {
+		next_keyer_state = KEY_DIT;
+	    }
+	    break;
+
+	case KEY_UP:
+	    switch(next_keyer_state) {
+	    case KEY_DIT:
+		next_state_transition_ms = now + dot_twitches;
+		tone(SIDETONE, SIDETONE_FREQUENCY);
+		break;
+
+	    case KEY_DAH:
+		next_state_transition_ms = now + dash_twitches;
+		tone(SIDETONE, SIDETONE_FREQUENCY);
+		break;
+
+	    default:
+		if (!digitalRead(LEFT_PADDLE)) {
+		    next_keyer_state = KEY_DIT;
+		    next_state_transition_ms = now + dot_twitches;
+		    tone(SIDETONE, SIDETONE_FREQUENCY);
+		}
+		else if (!digitalRead(RIGHT_PADDLE)) {
+		    next_keyer_state = KEY_DAH;
+		    next_state_transition_ms = now + dash_twitches;
+		    tone(SIDETONE, SIDETONE_FREQUENCY);
+		}
+		break;
+	    }
+	    keyer_state = next_keyer_state;
+	    next_keyer_state = KEY_UP;
+	    break;
 	}
     }
 }	
